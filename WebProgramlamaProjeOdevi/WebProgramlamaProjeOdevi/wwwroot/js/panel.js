26a05438a07ce85219464c669efd9b8e63cea8c3
yednz <59018879+yednz@users.noreply.github.com>
2021-01-09 01:45:30 +0300

    adminPanelHaberGirme

--- WebProgramlamaProjeOdevi/WebProgramlamaProjeOdevi/wwwroot/js/panel.js
+++ WebProgramlamaProjeOdevi/WebProgramlamaProjeOdevi/wwwroot/js/panel.js
@@ -1,54 +1,54 @@
-﻿/*
+﻿document.getElementById('getPosts').addEventListener('click', getPosts);
+document.getElementById('addPost').addEventListener('submit', addPost);
 
-Simple blog front end demo in order to learn AngularJS - You can add new posts, add comments, and like posts.
+function getPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((data) => {
+            let posts = '<h2 class="mb-4">Random Posts</h2>';
+            data.forEach((post) => {
+                posts += `
+      <div class="card card-body mb-3">
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+      </div>
+      `
+            });
+            document.getElementById('output').innerHTML = posts;
+        });
+}
 
-*/
+// post posts to the server
+function addPost(e) {
+    e.preventDefault();
 
-(function () {
-    var app = angular.module("blogApp", []);
+    let title = document.getElementById('title').value;
+    let body = document.getElementById('body').value;
 
-    app.controller("BlogController", [
-        "$http",
-        function ($http) {
-            var blog = this;
-            blog.title = "AngularJS Blog App";
-
-            blog.posts = {};
-            $http
-                .get("https://s3-us-west-2.amazonaws.com/s.cdpn.io/110131/posts_1.json")
-                .success(function (data) {
-                    blog.posts = data;
-                });
-
-            blog.tab = "blog";
-
-            blog.selectTab = function (setTab) {
-                blog.tab = setTab;
-                console.log(blog.tab);
-            };
-
-            blog.isSelected = function (checkTab) {
-                return blog.tab === checkTab;
-            };
-
-            blog.post = {};
-            blog.addPost = function () {
-                blog.post.createdOn = Date.now();
-                blog.post.comments = [];
-                blog.post.likes = 0;
-                blog.posts.unshift(this.post);
-                blog.tab = 0;
-                blog.post = {};
-            };
-        }
-    ]);
-
-    app.controller("CommentController", function () {
-        this.comment = {};
-        this.addComment = function (post) {
-            this.comment.createdOn = Date.now();
-            post.comments.push(this.comment);
-            this.comment = {};
-        };
-    });
-})();
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain , */*',
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: title,
+            body: body
+        })
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.title == '' && data.body == '') {
+                alert('please enter your title and content');
+            } else {
+                post = '<h4 class="mb-4">Your new post</h4>'
+                post += `
+        <div class="card card-body mb-3">
+          <h5>post title : ${data.title}</h5>
+          <p>post content : ${data.body}</p>
+        </div>
+        `
+                document.getElementById('output').innerHTML = post;
+            }
+        });
+}
