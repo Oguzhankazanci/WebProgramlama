0f568a76794a7101c7f1d86284d46b87f63760d7
yednz <59018879+yednz@users.noreply.github.com>
2021-01-08 22:25:59 +0300

    adminpanelgiris

---
+++ WebProgramlamaProjeOdevi/WebProgramlamaProjeOdevi/Controllers/yoneticiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProgramlamaProjeOdevi.Controllers
+{
+    public class yoneticiController:Controller
+    {
+        public IActionResult yonetimPanel()
+        {
+            return View();
+        }
+
+    }
+}
